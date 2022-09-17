524c0657de7445b813a896f52037f2a88128d70c
shaik <faqruddinsk11@gmail.com>
2022-09-17 11:34:16 +0530

    EcomPractice by first time commit and push

---
+++ src/test/java/Addtocart/ProductAddTocart.java
@@ -0,0 +1,12 @@
+package Addtocart;
+
+import org.testng.annotations.Test;
+
+public class ProductAddTocart {
+	@Test
+		public void addProduct() {
+			System.out.println("product added");
+		}
+	}
+
+
